f29727cf1ba0ccee955e7c3cc265ff39ca78b0b8
uncldrw <dreq2905@gmail.com>
2020-07-29 11:44:51 +0200

    edited html

--- sections/header.php
+++ sections/header.php
@@ -23,6 +23,14 @@ $resultCheck = mysqli_num_rows($result);
                <p>Teste Deine Tippgeschwindigkeit!</p>
             </div>
          </div>
+         <div>
+            <ul class="navPoints">
+               <li><a href="#">Custom Game</a></li>
+               <li><a href="#">Mein Account</a></li>
+               <li><a href="#">Anleitung</a></li>
+               <li><a href="#">Support</a></li>
+            </ul>
+         </div>
          <div class="headerRightNav">
             <?php
 if (isset($_SESSION['userId'])) {
